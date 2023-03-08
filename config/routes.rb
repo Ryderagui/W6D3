9c6205f8473fa6a0cf56795d5283e48d300a4fad
Ryder_Aguilera <ryderaguilera@hotmail.com>
2023-03-08 14:49:37 -0800

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # resources :users
+
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users$#update'
+
+end
