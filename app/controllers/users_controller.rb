5778ac5b6b66a73bec8ee9e3a3b411f000693410
Josh van Eyken <josh.vaneyken@gmail.com>
2023-03-08 17:32:27 -0800

    start update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
     end
     
     def show 
-        render json: params
+        input = User.find(params["id"])
+        render json: input
     end
 
     def create 
@@ -19,6 +20,11 @@ class UsersController < ApplicationController
             render json: user
         else 
             render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+
+        def update
+        input = User.find_by(id: params[:id])
+        redirect_to user_url(user.id)
         end
     end
 
